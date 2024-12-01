d037780176acd0f51de4cf3baec6031b8603ba96
PrinceAsare22 <asareprince764@gmail.com>
2024-12-01 03:13:12 -1200

    made some changes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -14,6 +14,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         toolbarHeight: 100,
         title: Center(child: Image.asset('lib/assets/vestimate_logo.png')),
